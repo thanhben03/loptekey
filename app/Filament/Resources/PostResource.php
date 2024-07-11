74f80f1a826425fbfc3cb40a287d57a207af2249
= <=>
2024-07-11 13:30:08 +0700

    fix edit post admin

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -27,7 +27,6 @@ class PostResource extends Resource
             ->schema([
 
                 Forms\Components\TextInput::make('title')
-                    ->label()
                     ->required(),
                 Forms\Components\Select::make('status')
                 ->options([
