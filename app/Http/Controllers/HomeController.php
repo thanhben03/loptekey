32d888774b442f62d7736ab37eda1a07626db667
= <=>
2024-07-12 09:55:25 +0700

    updateeee

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
         $now = Carbon::now();
         $month = $now->month;
         $year = $now->year;
-        $this->middleware(function ($request, $next) use ($now){
+        $this->middleware(function ($request, $next) use ($now, $month, $year) {
             $keyIds = Order::query()
             ->where('user_id', \auth()->user()->id)
             ->get()
@@ -30,7 +30,8 @@ class HomeController extends Controller
 
             $isTick = Key::query()
                 ->whereIn('id', $keyIds)
-                ->where('expired', '>=', $now)
+                ->whereMonth('expired', '>=', $month)
+                ->whereYear('expired', '>=', $year)
                 ->first();
             view()->share('isTick', !!$isTick);
             return $next($request);
