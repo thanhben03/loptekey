5a87e6ef6419ef31d7f3b2f8d955be4da667d944
= <=>
2024-07-15 21:00:04 +0700

    change text button back

--- resources/views/client/main-post.blade.php
+++ resources/views/client/main-post.blade.php
@@ -42,7 +42,7 @@
                             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                                 <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
                                 <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Ranking</button>
-                                <a href="https://loptephim.teebi.shop" class="nav-link">Quay lại</a>
+                                <a href="https://loptephim.teebi.shop" class="nav-link">Trang Chủ</a>
                             </div>
                         </nav>
                     </div>
