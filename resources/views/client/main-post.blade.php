38354f47bdd39978e939cfff25a272253f7e53c0
= <=>
2024-07-15 21:15:09 +0700

    fix header length

--- resources/views/client/main-post.blade.php
+++ resources/views/client/main-post.blade.php
@@ -43,10 +43,10 @@
                         </div>
                     </div>
                     <div class="col-xl-8 mb-4"
-                         style="
+                         >
+                        <nav style="
                             background: cornsilk
                          ">
-                        <nav>
                             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                                 <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
                                 <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Ranking</button>
