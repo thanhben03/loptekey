186a4bbe4b446c47d82a9d55862a6d78a3ac332c
= <=>
2024-07-26 20:03:45 +0700

    fix link home

--- resources/views/client/hide-post.blade.php
+++ resources/views/client/hide-post.blade.php
@@ -67,7 +67,7 @@
                             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                                 <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
                                 <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Ranking</button>
-                                <a href="https://loptephim.teebi.shop" class="nav-link">Trang Chủ</a>
+                                <a href="https://loptephim.xyz" class="nav-link">Trang Chủ</a>
 
                             </div>
                         </nav>
